c63a355da94b831f5541684d6e412b9293788e07
Fredrik Löwenhamn <lowet84@gmail.com>
2016-12-14 12:26:00 +0100

    Moved nodes.json

--- k8s-disaster-recovery-net-console/API/NodeController.cs
+++ k8s-disaster-recovery-net-console/API/NodeController.cs
@@ -1,10 +1,38 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using ServiceStack.Text;
 
 namespace k8s_disaster_recovery_net_console.API
 {
     public class NodeController : IController
     {
-        public object Response => new List<string> {"En sak"};
+        public object Response
+        {
+            get
+            {
+                if (Environment.OSVersion.Platform == PlatformID.Unix)
+                {
+                    try
+                    {
+                        var wc = new WebClient();
+                        var data = wc.DownloadString("http://localhost:8080/api/v1/nodes");
+                        File.WriteAllText(@"test.json",data);
+                        return data;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        throw e;
+                    }
+                }
+                else
+                {
+                    return new {Name = "Windows Dummy"};
+                }
+            }
+        }
 
         public string ContentType => "application/json";
     }
--- k8s-disaster-recovery-net-console/Program.cs
+++ k8s-disaster-recovery-net-console/Program.cs
@@ -7,10 +7,22 @@ namespace k8s_disaster_recovery_net_console
     {
         public static void Main(string[] args)
         {
-            using (Microsoft.Owin.Hosting.WebApp.Start<OwinStartup>("http://*:9000/"))
+            try
             {
-                Console.WriteLine("Press [enter] to quit...");
-                Console.ReadLine();
+                using (Microsoft.Owin.Hosting.WebApp.Start<OwinStartup>("http://*:9000/"))
+                {
+                    Console.WriteLine("Press [enter] to quit...");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                using (Microsoft.Owin.Hosting.WebApp.Start<OwinStartup>("http://localhost:9000/"))
+                {
+                    Console.WriteLine("Unable to accept requests from all hosts, reverting to localhost");
+                    Console.WriteLine("Press [enter] to quit...");
+                    Console.ReadLine();
+                }
             }
         }
     }
